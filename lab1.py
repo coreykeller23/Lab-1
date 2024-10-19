69c7221a1a4d3a410d2b19e23b19b3ecf3ddb8c6
coreykeller23 <167387751+coreykeller23@users.noreply.github.com>
2024-10-19 12:15:36 -0400

    Add files via upload
    This is a program that introduces myself. It is the first assignment for a beginner coding class.

---
+++ lab1.py
@@ -0,0 +1,20 @@
+"""
+Corey Keller
+Lab 1
+1.22.24
+"""
+
+
+print("My name is Corey Keller.")
+print("My major is Computer Science.")
+print("These are my hobbies:")
+print("  -Lifting")
+print("  -Cooking")
+print("  -Reading")
+print("")
+#Line 16 begins the I symbol with the * special character
+print("     *******")
+print("       ***  ")
+print("       ***  ")
+print("       ***  ")
+print("     *******")
